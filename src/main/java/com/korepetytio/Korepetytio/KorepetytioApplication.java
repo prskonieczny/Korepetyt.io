84b9709a37bd84870a7fd4b009c52370ee103f79
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2023-11-20 18:37:48 +0100

    Initial commit

---
+++ src/main/java/com/korepetytio/Korepetytio/KorepetytioApplication.java
@@ -0,0 +1,13 @@
+package com.korepetytio.Korepetytio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorepetytioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorepetytioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korepetytio/Korepetytio/KorepetytioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korepetytio.Korepetytio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorepetytioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
