fc8ebd809295fc106cb457d73e958104896b9982
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2023-11-21 15:41:23 +0100

    Basic entities for test, docker files

---
+++ src/main/java/com/korepetytio/Korepetytio/entities/AbstractEntity.java
@@ -0,0 +1,24 @@
+package com.korepetytio.Korepetytio.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/com/korepetytio/Korepetytio/entities/Account.java
@@ -0,0 +1,27 @@
+package com.korepetytio.Korepetytio.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Account extends AbstractEntity{
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+}
