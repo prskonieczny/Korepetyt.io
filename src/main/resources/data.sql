39a4bed737b6c843106d0a518ab49a59f6b351fc
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2023-11-23 23:59:21 +0100

    more inserts for data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,17 @@
+-- insert roles
 INSERT INTO role (permission_level) VALUES ('ADMIN');
 INSERT INTO role (permission_level) VALUES ('TEACHER');
 INSERT INTO role (permission_level) VALUES ('STUDENT');
 
+-- insert accounts -- all passwords: "password"
 INSERT INTO account (username, password, email, city, street, phone)
-    VALUES ('username', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'email@email.com', 'Lodz', 'Street', '123123123');
+    VALUES ('username', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'email@email.com', 'Lodz', 'Street', '111111111');
+INSERT INTO account (username, password, email, city, street, phone)
+    VALUES ('testteacher', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'teacheremail@email.com', 'Lodz', 'Street', '333333333');
+INSERT INTO account (username, password, email, city, street, phone)
+    VALUES ('teststudent', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'studentemail@email.com', 'Lodz', 'Street', '333333333');
 
-INSERT INTO account_role (account_id, roles_id) VALUES (1,1)
+-- insert account_roles
+INSERT INTO account_role (account_id, roles_id) VALUES (1,1);
+INSERT INTO account_role (account_id, roles_id) VALUES (2,2);
+INSERT INTO account_role (account_id, roles_id) VALUES (3,3);
