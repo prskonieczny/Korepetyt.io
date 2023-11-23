f5874dec06be416aacc441f792687eefe9eb2e75
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2023-11-24 00:12:31 +0100

    fix for data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO role (permission_level) VALUES ('STUDENT');
 
 -- insert accounts -- all passwords: "password"
 INSERT INTO account (username, password, email, city, street, phone)
-    VALUES ('username', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'email@email.com', 'Lodz', 'Street', '111111111');
+    VALUES ('testadmin', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'adminemail@email.com', 'Lodz', 'Street', '111111111');
 INSERT INTO account (username, password, email, city, street, phone)
     VALUES ('testteacher', '$2a$12$vIbiC0Fw0w/EvE1dnoZ.1udL4NI7HSw4UzbqVnBOOW6D.tYzWHuQK', 'teacheremail@email.com', 'Lodz', 'Street', '333333333');
 INSERT INTO account (username, password, email, city, street, phone)
