9a9ad790f3d01247bac8efd04431629d56bc88b9
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2023-11-25 19:59:59 +0100

    update for app.tsx

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -6,12 +6,14 @@ import RootLayout from './rootLayout/RootLayout';
 import HomePage from "./pages/HomePage";
 import LoginPage from './pages/Authorization/LoginPage';
 import RegisterStudentPage from './pages/Authorization/RegisterStudentPage';
+import RegisterTeacherPage from './pages/Authorization/RegisterTeacherPage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<RootLayout />}>
     <Route index element={<HomePage />} />
     <Route path="login" element={<LoginPage />} />
     <Route path="registerStudent" element={<RegisterStudentPage />} />
+    <Route path="registerTeacher" element={<RegisterTeacherPage />} />
   </Route>
 ))
 
