a1883e93593617cb59c49c0eac57d5936c2c22bb
Piotr Skonieczny <skonieczny.piotr01@gmail.com>
2024-01-19 00:23:00 +0100

    Small fix in TeacherStatistics

--- src/main/java/com/korepetytio/Korepetytio/entities/TeacherStatistics.java
+++ src/main/java/com/korepetytio/Korepetytio/entities/TeacherStatistics.java
@@ -1,26 +1,18 @@
 package com.korepetytio.Korepetytio.entities;
 
-import com.korepetytio.Korepetytio.entities.enums.Levels;
-import com.korepetytio.Korepetytio.entities.enums.Subjects;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 public class TeacherStatistics extends AbstractEntity{
     @Column(name = "number_of_lessons")
-    @NotNull
     private int numberOfLessons;
-    @Column(name = "teached_subjects")
-    private List<Subjects> techedSubjects = new ArrayList<>();
-    @Column(name = "teached_level")
-    private List<Levels> teachedLevels = new ArrayList<>();
     @OneToOne
     @JoinColumn(name = "teacher_id", referencedColumnName = "id")
     private Account teacher;
